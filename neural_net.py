1e7e4dfafec02313122a572472a89eef7117ecb1
Elias <86184580+eliaslimmer@users.noreply.github.com>
2025-07-13 19:55:35 +0200

    cleaned up neural_net class a bit

--- neural_net.py
+++ neural_net.py
@@ -1,58 +1,65 @@
 import numpy as np
+from activations import reLU, softmax, reLuDerivative
   
 
 class Neural_Network:
-    def __init__(self, inputLayerSize, outputLayerSize, hiddenLayerSize):
-      self.inputLayerSize = inputLayerSize
-      self.outputLayerSize = outputLayerSize
-      self.hiddenLayerSize = hiddenLayerSize
+    def __init__(self, input_layer_size, output_layer_size, hidden_layer_size, learning_rate=.1, optimizer=None):
+        self.optimizer = optimizer
+        self.learning_rate = learning_rate
 
-      # Weights and Bias
-      self.W1 = np.random.randn(self.hiddenLayerSize, self.inputLayerSize) * 0.01
-      self.W2 = np.random.randn(self.outputLayerSize, self.hiddenLayerSize) * 0.01
+        self.input_layer_size = input_layer_size
+        self.output_layer_size = output_layer_size
+        self.hidden_layer_size = hidden_layer_size
 
-      self.B1 = np.zeros((self.hiddenLayerSize, 1))
-      self.B2 = np.zeros((self.outputLayerSize, 1))
+        # Initialize weights with small random values to keep activations stable.
+        # He initialization might be better
+        self.W1 = np.random.randn(self.hidden_layer_size, self.input_layer_size) * 0.01
+        self.W2 = np.random.randn(self.output_layer_size, self.hidden_layer_size) * 0.01
+
+        self.b1 = np.zeros((self.hidden_layer_size, 1))
+        self.b2 = np.zeros((self.output_layer_size, 1))
 
 
     def forward(self, X):
-        self.Z1 = np.dot(self.W1, X) + self.B1
-        self.A1 = self.reLU(self.Z1)
+        """
+        X: (batch_size, input_size)
+        returns: (batch_size, output_size) probability matrix
+        """
+        self.Z1 = np.dot(self.W1, X) + self.b1
+        self.A1 = reLU(self.Z1)
 
-        self.Z2 = np.dot(self.W2, self.A1) + self.B2
-        self.A2 = self.softmax(self.Z2)
+        self.Z2 = np.dot(self.W2, self.A1) + self.b2
+        self.A2 = softmax(self.Z2)
 
         return self.A2
 
-    def backward(self, X, Y, learning_rate=.1):
+    def backward(self, X, Y):
+        """
+        X: (batch_size, input_size)
+        Y: (batch_size, output_size) one-hot labels
+        """
+
         m = X.shape[1]
 
         dZ2 = self.A2 - Y
 
         dW2 = (1 / m) * np.dot(dZ2, self.A1.T)
-        dB2 = (1 / m) * np.sum(dZ2, axis=1, keepdims=True)
+        db2 = (1 / m) * np.sum(dZ2, axis=1, keepdims=True)
 
         dA1 = np.dot(self.W2.T, dZ2)
-        dZ1 = dA1 * self.reLuDerivative(self.Z1)
+        dZ1 = dA1 * reLuDerivative(self.Z1)
 
         dW1 = (1 / m) * np.dot(dZ1, X.T)
-        dB1 = (1 / m) * np.sum(dZ1, axis=1, keepdims=True)
+        db1 = (1 / m) * np.sum(dZ1, axis=1, keepdims=True)
 
-        self.W2 -= learning_rate * dW2
-        self.B2 -= learning_rate * dB2
-        self.W1 -= learning_rate * dW1
-        self.B1 -= learning_rate * dB1
+        if self.optimizer:
+            self.optimizer.update()
+        else:
+            self.update_parameters(dW1, db1, dW2, db2)
+  
 
-
-    def reLU(self, Z):
-        return np.maximum(0, Z)
-
-    def reLuDerivative(self, Z):
-        return Z > 0
-
-    def softmax(self, Z):
-        expZ = np.exp(Z - np.max(Z, axis=0, keepdims=True))
-        return expZ / np.sum(expZ, axis=0, keepdims=True)
-
-    def softmaxDerivative(self, Z):
-        return self.softmax(Z) * (1 - self.softmax(Z))
+    def update_parameters(self, dW1, db1, dW2, db2):
+        self.W1 -= self.learning_rate * dW1
+        self.b1 -= self.learning_rate * db1
+        self.W2 -= self.learning_rate * dW2
+        self.b2 -= self.learning_rate * db2
