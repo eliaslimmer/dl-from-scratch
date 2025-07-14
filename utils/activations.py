8e69d773ccb7e8d1c5380fb4fbb779f97dac4906
Elias <86184580+eliaslimmer@users.noreply.github.com>
2025-07-14 14:15:02 +0200

    fixed relu÷

--- utils/activations.py
+++ utils/activations.py
@@ -1,14 +1,14 @@
 import numpy as np
 
-def reLU(self, Z):
+def reLU(Z):
     return np.maximum(0, Z)
 
-def reLuDerivative(self, Z):
+def reLuDerivative(Z):
     return Z > 0
 
-def softmax(self, Z):
+def softmax(Z):
     expZ = np.exp(Z - np.max(Z, axis=0, keepdims=True))
     return expZ / np.sum(expZ, axis=0, keepdims=True)
 
-def softmaxDerivative(self, Z):
-    return self.softmax(Z) * (1 - self.softmax(Z))
+def softmaxDerivative(Z):
+    return softmax(Z) * (1 - softmax(Z))
