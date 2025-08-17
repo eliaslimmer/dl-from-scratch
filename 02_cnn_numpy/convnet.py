5b608d229fce59d24da2325b379f069e741a29e7
Elias <86184580+eliaslimmer@users.noreply.github.com>
2025-08-17 12:07:39 +0200

    added structure

---
+++ 02_cnn_numpy/convnet.py
@@ -0,0 +1,72 @@
+import numpy as np
+
+
+# ==== utils ====
+
+
+
+
+# ==== layers ====
+
+class Conv2D:
+    def __init__(self):
+        pass
+
+    def foreward(self):
+        pass
+
+    def backward(self):
+        pass
+
+
+class ReLu:
+    def foreward(self):
+        pass
+
+    def backward(self):
+        pass
+
+
+class MaxPool2D:
+    def foreward(self):
+        pass
+
+    def backward(self):
+        pass
+
+class Flatten:
+    def foreward(self):
+        pass
+
+    def backward(self):
+        pass
+
+
+class Dense:
+    def __init__(self):
+        pass
+
+    def foreward(self):
+        pass
+
+    def backward(self):
+        pass
+
+
+# ==== Network ====
+
+class SimpleCNN:
+    def __init__(self):
+        pass
+
+    def foreward(self):
+        pass
+
+    def backward(self):
+        pass
+
+
+# ==== Trainings loop ====
+
+def train():
+    pass
